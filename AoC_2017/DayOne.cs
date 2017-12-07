ca73ac2be5c5de449efc864796dc21fc45af8a6a
cwright <chris@cwsquared.com>
2017-12-07 16:40:17 -0500

    Initial commit. Day 1 (all) and Day 2 (part 1).

---
+++ AoC_2017/DayOne.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AoC_2017
+{
+    class DayOne
+    {
+        public static void PartOne(string input)
+        {
+            int total = 0;
+            Char previousDigit = input[input.Length - 1];
+            Char currentDigit = ' ';
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentDigit = input[i];
+
+                if (previousDigit.Equals(currentDigit))
+                {
+                    total += (int)Char.GetNumericValue(currentDigit);
+                }
+                previousDigit = currentDigit;
+            }
+
+            Console.WriteLine("The Day One - Part One solution is: " + total.ToString());
+        }
+
+        public static void PartTwo(string input)
+        {
+            int total = 0;
+            int half = input.Length / 2;
+            var currentDigit = ' ';
+            var comparisonDigit = ' ';
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentDigit = input[i];
+
+
+                if ((i + half <= input.Length - 1))
+                {
+                    comparisonDigit = input[i + half];
+                }
+                else
+                {
+                    comparisonDigit = input[i - half];
+                }
+
+                if (comparisonDigit.Equals(currentDigit))
+                {
+                    total += (int)Char.GetNumericValue(currentDigit);
+                }
+                comparisonDigit = currentDigit;
+            }
+
+            Console.WriteLine("The Day One - Part Two solution is: " + total.ToString());
+        }
+    }
+}
---
+++ AoC_2017/DayTwo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AoC_2017
+{
+    class DayTwo
+    {
+        public static void PartOne(string[] input)
+        {
+            int checksum = 0;
+
+            foreach (string line in input)
+            {
+                int high = int.MinValue;
+                int low = int.MaxValue;
+                string[] values = line.Split('\t');
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    int currentValue = int.Parse(values[i]);
+                    if (currentValue >= high)
+                    {
+                        high = currentValue;
+                    }
+                    else if (currentValue < low)
+                    {
+                        low = currentValue;
+                    }
+                }
+
+                checksum += (high - low);
+            }
+
+            Console.WriteLine($"The Day 2, Part 1 solution is: {checksum}");
+        }
+
+        public static void PartTwo(string[] input)
+        {
+            int sum = 0;
+
+
+
+            Console.WriteLine($"The Day 2, Part 1 solution is: {sum}");
+        }
+    }
+}
